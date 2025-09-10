af12df4dc6a0d8405f93024fc306dd5fe11399fa
miroslavpojer <miroslavpojer@seznam.cz>
2025-09-10 19:09:59 +0200

    Backup of latest changes.

--- pipelines/p00_raw_data.py
+++ pipelines/p00_raw_data.py
@@ -11,3 +11,9 @@ logger = logging.getLogger(__name__)
 
 def run_pipeline(cfg: dict):
     logger.debug("Running raw data pipeline with config: %s", cfg)
+
+    # pokracuj pripravou raw dat do cilove pozice a 00 slozky, odkud si to muzou vzit dalsi
+    #   - daily, weekly closed price - staci jen 2 akcie !!! (AAPL, MSFT) ???
+    #  TODO - co je to adjusted close?
+    #   - earnings jsou dulezite
+    
